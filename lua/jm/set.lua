1b4686aba88cdd462fd00c4c0867f8e3a0f09663
João Mário <joaomf17p2@gmail.com>
2023-04-24 20:52:35 +0000

    setting background as transparent

--- lua/jm/set.lua
+++ lua/jm/set.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
 
 vim.cmd.colorscheme "catppuccin"
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 vim.opt.number = true
 vim.opt.relativenumber = true
