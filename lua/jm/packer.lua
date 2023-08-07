785ff14bab19366d13097ea0e5550e74028fac82
João Mário <joaomf17p2@gmail.com>
2023-08-07 19:37:55 -0300

    fixing lsp configuration

--- lua/jm/packer.lua
+++ lua/jm/packer.lua
@@ -35,12 +35,16 @@ return require('packer').startup(function(use)
 			{ 'neovim/nvim-lspconfig' }, 
 			{
 				'williamboman/mason.nvim',
-				run = function() pcall(vim.cmd, 'MasonUpdate') end
+				run = function()
+					pcall(vim.cmd, 'MasonUpdate')
+				end
 			},
+			{'williamboman/mason-lspconfig.nvim'},
 			{'hrsh7th/nvim-cmp'}, 
 			{'hrsh7th/cmp-nvim-lsp'},
 			{'L3MON4D3/LuaSnip'}, 
 		}
 	}
+
 end)
 
