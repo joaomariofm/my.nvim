35290f818134b652b282bb811f467b5fcc009924
João Mário <joaomf17p2@gmail.com>
2023-08-07 19:29:06 -0300

    removing markdown preview plugin

--- lua/jm/packer.lua
+++ lua/jm/packer.lua
@@ -23,13 +23,6 @@ return require('packer').startup(function(use)
 	}
 
 	use {
-		"iamcco/markdown-preview.nvim",
-		run = "cd app && npm install",
-		setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
-		ft = { "markdown" }
-	}
-
-	use {
 		"akinsho/toggleterm.nvim",
 		tag = '*',
 		config = function() require("toggleterm").setup() end
