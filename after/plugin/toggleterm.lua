81ff36cc5db56fd7e892d2ffbbf0ddca946a4d53
João Mário <joaomf17p2@gmail.com>
2023-12-29 10:39:58 -0300

    adjusting toggleterm

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,28 +1,23 @@
 local status_ok, toggleterm = pcall(require, "toggleterm")
+
 if not status_ok then
 	return
 end
 
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
+vim.api.nvim_set_keymap('t', '<C-l>', '<Cmd>wincmd l<CR>', {noremap = true})
+vim.api.nvim_set_keymap('t', '<C-h>', '<Cmd>wincmd h<CR>', {noremap = true})
+
 toggleterm.setup({
 	size = 20,
-	open_mapping = [[<c-w>]],
-	hide_numbers = true,
-	shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = 2,
+	open_mapping = [[<leader>tt]],
+	direction = "horizontal",
+
 	start_in_insert = true,
 	insert_mappings = true,
 	persist_size = true,
-	direction = "float",
 	close_on_exit = true,
 	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
-		winblend = 0,
-		highlights = {
-			border = "Normal",
-			background = "Normal",
-		},
-	},
 })
+
 
