5731e0cb31b8823aa53cc448c122c58b9cd6f0e2
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-02-11 13:56:02 -0300

    adding lsp_zero and treesitter

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,19 @@
+local lsp_zero = require('lsp-zero')
+local mason = require('mason')
+local mason_lspconfig = require('mason-lspconfig')
+
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+mason.setup({})
+mason_lspconfig.setup({
+	ensure_installed = {
+		'tsserver',
+		'vtsls',
+		'eslint'
+	},
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -15,8 +15,20 @@ return require('packer').startup(function(use)
 	use { 'nvim-lualine/lualine.nvim' }
 
 	-- comment
-	use { 'numToStr/Comment.nvim' }	
+	use { 'numToStr/Comment.nvim' }
 
 	-- github copilot
 	use { "github/copilot.vim" }
+
+	-- lsp zero
+	use { 'williamboman/mason.nvim' }
+  use { 'williamboman/mason-lspconfig.nvim' }
+	use { 'neovim/nvim-lspconfig' }
+	use { 'hrsh7th/nvim-cmp' }
+	use { 'hrsh7th/cmp-nvim-lsp' }
+	use { 'L3MON4D3/LuaSnip' }
+	use { 'VonHeikemen/lsp-zero.nvim' }
+
+	-- treesitter
+	use { 'nvim-treesitter/nvim-treesitter' }
 end)
