aa2eccc6c1d47b97c663ae85a59c3552373289dc
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-02-12 16:49:17 -0300

    refactoring packer file syntax

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -1,45 +1,45 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use { 'wbthomason/packer.nvim' }
+	use 'wbthomason/packer.nvim'
 
 	-- web-devicons
-	use { 'nvim-tree/nvim-web-devicons' }
+	use 'nvim-tree/nvim-web-devicons'
 
 	-- telescope
-	use { 'nvim-lua/plenary.nvim' }
-	use { 'nvim-telescope/telescope.nvim' }
+	use 'nvim-lua/plenary.nvim'
+	use 'nvim-telescope/telescope.nvim'
 
 	-- gruvbox colorscheme
-	use { 'ellisonleao/gruvbox.nvim' }
+	use 'ellisonleao/gruvbox.nvim'
 
 	-- lualine
-	use { 'nvim-lualine/lualine.nvim' }
+	use 'nvim-lualine/lualine.nvim'
 
 	-- comment
-	use { 'numToStr/Comment.nvim' }
+	use 'numToStr/Comment.nvim'
 
 	-- github copilot
-	use { 'github/copilot.vim' }
+	use 'github/copilot.vim'
 
 	-- lsp zero
-	use { 'williamboman/mason.nvim' }
-	use { 'williamboman/mason-lspconfig.nvim' }
-	use { 'neovim/nvim-lspconfig' }
-	use { 'hrsh7th/nvim-cmp' }
-	use { 'hrsh7th/cmp-nvim-lsp' }
-	use { 'L3MON4D3/LuaSnip' }
-	use { 'VonHeikemen/lsp-zero.nvim' }
+	use 'williamboman/mason.nvim'
+	use 'williamboman/mason-lspconfig.nvim'
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'L3MON4D3/LuaSnip'
+	use 'VonHeikemen/lsp-zero.nvim'
 
 	-- treesitter
-	use { 'nvim-treesitter/nvim-treesitter' }
+	use 'nvim-treesitter/nvim-treesitter'
 
 	-- toggleterm
-	use { 'akinsho/toggleterm.nvim' }
+	use 'akinsho/toggleterm.nvim'
 
 	-- nvimtree
-	use { 'nvim-tree/nvim-tree.lua' }
+	use 'nvim-tree/nvim-tree.lua'
 
 	-- fugitive
-	use { 'tpope/vim-fugitive' }
+	use 'tpope/vim-fugitive'
 end)
