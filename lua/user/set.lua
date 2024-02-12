a90f80ac66b54a1c086cffcf8e0685393cfa43f9
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-02-12 16:45:53 -0300

    solving invisible cursor problem

--- lua/user/set.lua
+++ lua/user/set.lua
@@ -3,8 +3,6 @@ vim.g.mapleader = ' '
 -- transparency cases
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'TermCursor', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'TermCursorNC', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'CursorLine', { bg = 'none' })
 
