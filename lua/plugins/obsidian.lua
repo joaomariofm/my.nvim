c62f8c5813e601a90b5b693fa980f62773d518e5
João Mário <joaomf17p2@gmail.com>
2024-02-28 16:02:12 -0300

    adjusting obsidian note formatter

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -4,7 +4,18 @@ require("obsidian").setup({
 			name = "personal",
 			path = "~/Documentos/SB",
 		},
-	}
+	},
+	note_frontmatter_func = function(note)
+		local out = {}
+
+		if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+      for k, v in pairs(note.metadata) do
+        out[k] = v
+      end
+    end
+
+		return out
+	end,
 })
 
 vim.opt.conceallevel = 2
