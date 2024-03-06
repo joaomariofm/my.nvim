c5a4545ca01d2993b1f0e258af3bc5f2cfe8d104
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-03-05 23:38:07 -0300

    updating treesitter ensure installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,9 +2,11 @@ require('nvim-treesitter.configs').setup {
   ensure_installed = {
 		'c',
 		'javascript',
+		'typescript',
 		'vim',
 		'vimdoc',
-		'query'
+		'query',
+		'css',
 	},
   sync_install = false,
   auto_install = true,
