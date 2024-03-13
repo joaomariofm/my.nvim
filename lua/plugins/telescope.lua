02d6942e81b62a049775db91d1784ae5f5cb712c
João Mário Fidelis Martins <joao.martins@soluti.com.br>
2024-03-13 17:46:17 -0300

    feat: adjusting telescope search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,16 @@
 local telescope = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
+local function config_search(func_name, search_opts)
+	return function()
+		require('telescope.builtin')[func_name]({
+			prompt_prefix = ' search: ',
+			find_command = search_opts,
+			file_ignore_patterns = { 'node_modules', '.git', '.lock' },
+		})
+	end
+end
+
+vim.keymap.set('n', '<leader>ff', config_search('find_files', { 'rg', '--files', '--hidden', '-uu' }), {})
+vim.keymap.set('n', '<leader>fg', config_search('live_grep', { 'rg', '--hidden', '-uu' }), {})
 vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
 vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
