2c41599b0f770c3b0d44e8f63b382565483341b0
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-05-11 18:29:39 -0300

    adding markdown preview

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -48,4 +48,19 @@ return require('packer').startup(function(use)
 
 	-- obsidian -- requires -- use 'nvim-lua/plenary.nvim'
 	use 'epwalsh/obsidian.nvim'
+
+	-- markdown preview
+	use 'iamcco/markdown-preview.nvim'
+	--
+	-- Requires manual configuration as follows:
+	--
+	-- (over node v16.20.0)
+	--
+	-- cd ~/.local/share/nvim/site/pack/packer/start/
+	-- git clone https://github.com/iamcco/markdown-preview.nvim.git
+	-- cd markdown-preview.nvim
+	-- npx --yes yarn install
+	-- npx --yes yarn build
+	--
+
 end)
