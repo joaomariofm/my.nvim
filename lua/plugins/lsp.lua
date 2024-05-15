2a190b115f0fa06a1708befecb103acf457be61d
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-05-14 23:11:42 -0300

    adjusting lsp config file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,17 +1,14 @@
 local lsp_zero = require('lsp-zero')
-local mason = require('mason')
-local mason_lspconfig = require('mason-lspconfig')
 
 lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-mason.setup({})
-mason_lspconfig.setup({
-	ensure_installed = {
-		'tsserver',
-	},
+require('mason').setup({})
+require('mason-lspconfig').setup({
   handlers = {
-    lsp_zero.default_setup,
-  },
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  }
 })
