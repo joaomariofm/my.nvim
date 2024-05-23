1cbb95fe4242702e66c4108b6268600f62019998
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-05-23 00:29:00 -0300

    adding 'ensure installed' lsps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,6 +6,12 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
+	ensure_installed = {
+		'tsserver',
+		'cssls',
+		'html',
+		'lua_ls',
+	},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
